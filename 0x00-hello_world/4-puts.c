fc5812f365f2b5b112311a770f5bb108d3048584
charles-ui-cyber <okello>
2023-06-21 22:30:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
  /**
-  * main -Entry point
-  *
+  * main - Aprogram that prints a line with pits function
   * Return: Always 0 (Sucess)
   */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle\n");
+        return (0);
 }
 
