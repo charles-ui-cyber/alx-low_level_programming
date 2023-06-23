fb3c0ad4013d64ed1f172eec0f13f181aa83d4bb
charles-ui-cyber <okello>
2023-06-22 22:26:27 -0700

    101-qoute.c

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -2,13 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * This program prints the line "and that piece of art is
- * useful - Dora Korpar, 2015-10-19"
- * Return: Always 1 (Success)
+ * This program is not "allowed to use any functions listed in the NAME"
+ * * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that pieace of art is useful\" -Dors korper, 2015-10-19\n", 59);
 return (1);
 }
