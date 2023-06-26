4c145a22b6bba519b38a81f2464bec8938bc1899
charles-ui-cyber <okello>
2023-06-25 17:14:50 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
 {
-if (!((ones == tens) || (tens == hundreds) ||(tens > ones) ||
+if (!((ones == tens) || (tens == hundreds) || (tens > ones) ||
 (hundreds > tens)))
 {
 putchar(hundreds);
