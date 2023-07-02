9da49c9525b3f2554190b8b690c9bd6474201930
charles-ui-cyber <okello>
2023-07-02 00:39:31 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c);
+
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    putchar(r + '0');
+    r = _isalpha('o');
+    putchar(r + '0');
+    r = _isalpha(108);
+    putchar(r + '0');
+    r = _isalpha(';');
+    putchar(r + '0');
+    putchar('\n');
+    return (0);
+}
